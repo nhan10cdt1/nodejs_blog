4086dd0fe0a0888453b637c807d5761d6d60d9ab
nhan10cdt1 <nhanbkdn1211@gmail.com>
2024-12-15 17:59:41 +0900

    23. Prettier - Code formatter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
-const newsRouter = require("./news.js"); // Import module router cho phần news
-const siteRouter = require("./site.js"); // Import module router cho phần site
+const newsRouter = require('./news.js'); // Import module router cho phần news
+const siteRouter = require('./site.js'); // Import module router cho phần site
 
 function route(app) {
-  // Gắn route "/news" với newsRouter
-  // Mọi yêu cầu bắt đầu bằng "/news" sẽ được xử lý bởi newsRouter
-  app.use("/news", newsRouter);
-  app.use("/", siteRouter);
+    // Gắn route "/news" với newsRouter
+    // Mọi yêu cầu bắt đầu bằng "/news" sẽ được xử lý bởi newsRouter
+    app.use('/news', newsRouter);
+    app.use('/', siteRouter);
 }
 
 module.exports = route; // Xuất hàm route để sử dụng trong các phần khác của ứng dụng
